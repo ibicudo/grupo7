35e3c02dc9a2d663f78655c8c3df933281825f7a
Ana Barbara Cacheado Seabra <ana.cacheado@mercadolivre.com>
2021-05-20 12:01:51 -0300

    exercicio 1

--- modulo5/src/exercicio1/Exercicio1.java
+++ modulo5/src/exercicio1/Exercicio1.java
@@ -1,7 +1,18 @@
 package exercicio1;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um valor:");
+        int n = scanner.nextInt();
+
+        for(int i = 0; i < n; i++){
+            System.out.println(i*2);
+        }
     }
 }
