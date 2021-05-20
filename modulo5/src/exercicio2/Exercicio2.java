40567bfc744b92f1cd74870cc290c6837149260c
Ana Barbara Cacheado Seabra <ana.cacheado@mercadolivre.com>
2021-05-20 12:10:19 -0300

    exercicio 2

---
+++ modulo5/src/exercicio2/Exercicio2.java
@@ -0,0 +1,22 @@
+package exercicio2;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe a quantidade de numeros:");
+        int n = scanner.nextInt();
+
+        System.out.println("Informe um valor para verificar seus multiplos:");
+        int m = scanner.nextInt();
+
+        for(int i = 1; i < n+1; i++){
+
+            System.out.println(i*m);
+        }
+    }
+}
