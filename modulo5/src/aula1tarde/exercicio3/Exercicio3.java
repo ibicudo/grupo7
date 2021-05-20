d0284695331ae01e8d069ff5220aaddd2babea74
Fernando Axthelm <fernando.axthelm@mercadolivre.com>
2021-05-20 17:02:13 -0300

    aula 1 - tarde - exercicio 3

---
+++ modulo5/src/aula1tarde/exercicio3/Exercicio3.java
@@ -0,0 +1,36 @@
+package modulo5.src.aula1tarde.exercicio3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        HashMap<Integer, String> products = new HashMap<>();
+        HashMap<Integer, Double> prices = new HashMap<>();
+        HashMap<Integer, Integer> quantitys = new HashMap<>();
+
+
+        double total = 0;
+
+        for(int i = 1; i < 4; i++){
+            products.put(i, scanner.next());
+            prices.put(i, scanner.nextDouble());
+            quantitys.put(i,scanner.nextInt());
+            total += prices.get(i) * quantitys.get(i);
+        }
+        for(int i = 1; i < 4; i++){
+            System.out.println("Produto " + i);
+            System.out.println(products.get(i));
+            System.out.println("R$" + prices.get(i));
+            System.out.println("Quantidade: " + quantitys.get(i));
+            System.out.println();
+        }
+
+        System.out.println("Valor Total: R$" +total);
+    }
+}
