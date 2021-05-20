ad83733eb6d114ee75f9179a6062c39e6bd9a46f
Ana Barbara Cacheado Seabra <ana.cacheado@mercadolivre.com>
2021-05-20 12:47:44 -0300

    exercicio 3 - imcompleto

---
+++ modulo5/src/exercicio4/Exercicio4.java
@@ -0,0 +1,38 @@
+package exercicio4;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um número:");
+
+        int n = scanner.nextInt();
+
+        int numeros = 0;
+        int i = 2;
+
+        while (numeros < n) {
+
+            boolean verificandoPrimo = true;
+            for (int j = 2; j < i / 2; j++) {
+
+                if (i % j == 0) {
+                    verificandoPrimo = false;
+                    break;
+                }
+
+            }
+
+            if (verificandoPrimo) {
+                System.out.println(i);
+                numeros++;
+            }
+
+            i++;
+        }
+    }
+}
